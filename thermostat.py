8407ec38c8bf134d7e3761e9abccad73285c62e5
Zaharia BURGHELEA <legendaryproyi@yahoo.com>
2022-11-21 22:02:13 +0200

    Added sensor class that encapsulates the Thermostat to use only a default min/max value. It also provides a way to check a default number of time_steps.

--- thermostat.py
+++ thermostat.py
@@ -1,22 +1,23 @@
 import matplotlib.pyplot as plt, random
 
 class Thermostat:
-    __power = 0.5
     __heat = False
     __cool = False
 
-    def __init__(self, temperature, min, max):
+    def __init__(self, temperature, min, max, temperature_variance):
         self.temperature = temperature
         assert(min < max)
         self.min = min
         self.max = max
+        self.power_variance = 2*temperature_variance #the heating/cooling is twice more powerful than the temperature variance
+        self.temperature_variance = temperature_variance
 
-    def get_temperature(self, temperature):
+    def set_temperature(self, temperature):
         self.temperature = temperature
 
-        if self.temperature < self.min:
+        if self.temperature < (self.min + self.temperature_variance):
             self.__heat = True
-        elif self.temperature > self.max:
+        elif self.temperature > (self.max - self.temperature_variance):
             self.__cool = True
 
         if self.__heat and self.temperature > (self.min + self.max)/2:
@@ -29,24 +30,31 @@ class Thermostat:
         elif self.__cool:
             self.cooling()
 
+
     def heating(self):
-        self.temperature += self.__power
+        self.temperature += self.power_variance
 
     def cooling(self):
-        self.temperature -= self.__power
+        self.temperature -= self.power_variance
+
+
+class Sensor:
+    __temperature_variance = 0.3 #default variation of temperature between two time-steps
+    temperature_history = []
+
+    def __init__(self, min, max):
+        self.thermostat = Thermostat(random.randint(min, max), min, max, self.__temperature_variance)
+
+    def run_time_steps(self, time_steps):
+        for i in range(time_steps):
+            self.temperature_history += [self.thermostat.temperature]
+            self.thermostat.set_temperature(self.thermostat.temperature + self.__temperature_variance*(random.random() - 0.5)/0.5)
+
 
 
 if __name__ == '__main__':
-    test = Thermostat(35, 20, 25)
-    plot_list = []
-    
-    for i in range(1000):
-        if i % 100 == 0:
-            test.temperature = 30
-        if i % 200 == 0:
-            test.temperature = 15
-        plot_list += [test.temperature]
-        test.get_temperature(test.temperature + random.random() - 0.5)
+    test_sensor = Sensor(20, 25)
+    test_sensor.run_time_steps(1000)
 
-    plt.plot(plot_list)
+    plt.plot(test_sensor.temperature_history)
     plt.show()
